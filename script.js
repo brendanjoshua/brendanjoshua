56db69fd80e4da1aedda6d3c16cb931e7c534e7c
brendanjoshua <163716178+brendanjoshua@users.noreply.github.com>
2025-02-03 16:00:21 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const person = {
+    name: "Brendan Acuna",
+    age: 21,
+    hobbies: ["reading", "coding", "gaming"]
+  };
+  
+  const jsonString = JSON.stringify(person);
+  console.log(jsonString);
