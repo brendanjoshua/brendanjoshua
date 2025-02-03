fb9b950d83c292a126069f259a99f1aa3e288869
brendanjoshua <163716178+brendanjoshua@users.noreply.github.com>
2025-02-03 16:07:22 +0800

    Create scriptdata.js

---
+++ scriptdata.js
@@ -0,0 +1,14 @@
+fetch('https://randomuser.me/api/')
+    .then(response => response.json())
+    .then(data => {
+        const user = data.results[0];
+        const userDataDiv = document.getElementById('userData');
+        userDataDiv.innerHTML = `
+            <p>Name: ${user.name.first} ${user.name.last}</p>
+            <p>Email: ${user.email}</p>
+            <p>Location: ${user.location.city}, ${user.location.country}</p>
+            <p>Phone: ${user.phone}</p>
+            <img src="${user.picture.medium}" alt="User Image">
+        `;
+    })
+    .catch(error => console.error('Error fetching data:', error));
